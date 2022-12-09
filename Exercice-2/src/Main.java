3514a9d6ad5a0271ddaef40ffba30ed344608daf
PierreBoue <119578551+PierreBoue@users.noreply.github.com>
2022-12-09 10:36:14 +0100

    exo 2-1

--- Exercice-2/src/Main.java
+++ Exercice-2/src/Main.java
@@ -3,10 +3,12 @@ public class Main
     public static void main(String[] args)
     {
         //System.out.println("Hello world!");
-
+        System.out.println( petit_carre(30));
     }
-    public static int petit_carre()
+    public static int petit_carre( int n )
     {
-
+        double resultat = Math.floor( Math.sqrt(n));
+        if ( Math.pow(resultat, 2) == n) return n;
+        return (int)Math.pow(resultat + 1, 2);
     }
 }
