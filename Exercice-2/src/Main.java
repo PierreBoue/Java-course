4a56e99da0337346b9c3cd9ac82a2706558fc677
PierreBoue <119578551+PierreBoue@users.noreply.github.com>
2022-12-09 11:04:45 +0100

    finished exercice 2

--- Exercice-2/src/Main.java
+++ Exercice-2/src/Main.java
@@ -2,8 +2,8 @@ public class Main
 {
     public static void main(String[] args)
     {
-        //System.out.println("Hello world!");
-        System.out.println( petit_carre(30));
+        //System.out.println( petit_carre(30));
+        System.out.println( grand_carre(63));
     }
     public static int petit_carre( int n )
     {
@@ -11,4 +11,10 @@ public class Main
         if ( Math.pow(resultat, 2) == n) return n;
         return (int)Math.pow(resultat + 1, 2);
     }
+    public static int grand_carre( int n )
+    {
+        double resultat = Math.floor( Math.sqrt(n));
+        return (int)Math.pow(resultat, 2);
+    }
+
 }
