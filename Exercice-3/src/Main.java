7baab99dfc5d4eac5baf3e27caa3b50772e0acad
PierreBoue <119578551+PierreBoue@users.noreply.github.com>
2022-12-09 14:49:35 +0100

    Exercice 3 -2

--- Exercice-3/src/Main.java
+++ Exercice-3/src/Main.java
@@ -4,13 +4,14 @@ public class Main
 {
     public static void main(String[] args)
     {
-        int[] suite = fibo(10 );
-        for (int n: suite) System.out.println(n);
-
+        //int[] suite = fibo(10 );
+        // for (int n: suite) System.out.println(n);
+        int[] aprox = approximation_fibo((float)0.01 );
+        System.out.println("a = " + aprox[0] +" b = " + aprox[1]);
     }
     private static int[] fibo( int nbre)
     {
-        ArrayList<Integer> suite = new ArrayList<Integer>();
+        ArrayList<Integer> suite = new ArrayList<Integer>();//initialise un
         suite.add(0);
         suite.add(1);
         int idx = 2;
@@ -22,8 +23,20 @@ public class Main
         int[] rez = suite.stream().mapToInt(i ->i).toArray();
         return rez;
     }
-    private static int[] approximation_fibo()
+    private static int[] approximation_fibo( float epsilon )
     {
-        
+        final double phi = ( 1 + Math.sqrt(5)) / 2;
+        ArrayList<Integer> fibo = new ArrayList();
+        int f1 = 1;
+        int f2 = 1;
+
+        while ( Math.abs(phi - (float)f2 / (float)f1 ) > epsilon )
+        {
+            int temp = f1;
+            f1 = f2;
+            f2 = temp + f2;
+        }
+        int[] retour = { f2, f1 };
+        return retour;
     }
 }
