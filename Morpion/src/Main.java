7e70ea20cc729bf145a9cd868445a12bad87bf94
PierreBoue <119578551+PierreBoue@users.noreply.github.com>
2022-12-12 11:18:33 +0100

    moved Morpion

--- Morpion/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main
-{
-    public static void main(String[] args)
-    {
-       TicTacToe morpion = new TicTacToe();
-       morpion.display();
-       morpion.getMoveFromPlayer();
-    }
-}
