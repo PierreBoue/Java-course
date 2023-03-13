5e1fc0599acb44645b15c589ce9ad0ab05aadc2e
Ludovic PEYSSON <ludovic.peysson@Digital-Grenoble.local>
2023-03-13 15:34:48 +0100

    new class in ex4

---
+++ Exercice-4/src/ToUpperCase.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class ToUpperCase {
+
+    static void toUpperCase(ArrayList<String> prenoms)
+    {
+        for ( String prenom: prenoms ) System.out.println(prenom.toUpperCase());
+    }
+}
